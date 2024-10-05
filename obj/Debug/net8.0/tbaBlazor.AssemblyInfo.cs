004571ff384b09cab4ac1c1d57a67d076bd9708f
Stephen Davidson <stephen@ventil8.com>
2024-10-05 21:45:27 +0100

    Contact form

--- obj/Debug/net8.0/tbaBlazor.AssemblyInfo.cs
+++ obj/Debug/net8.0/tbaBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tbaBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12e62efc5905275ae6bc037e7df2da9ea70630c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5b51a636116e3c0bb2a7509918d921d9b77c6b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("tbaBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tbaBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
