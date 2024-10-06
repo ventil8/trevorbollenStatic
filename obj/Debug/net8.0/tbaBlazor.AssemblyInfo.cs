45e9744eef731419f0d078d9c384ef112cdaab93
Stephen Davidson <stephen@ventil8.com>
2024-10-06 11:53:21 +0100

    SEO improvemtns

--- obj/Debug/net8.0/tbaBlazor.AssemblyInfo.cs
+++ obj/Debug/net8.0/tbaBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tbaBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70f465810d1c97da2d068de823910f85563789e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+030d603297b2952c17322ce469391d4df8538a9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("tbaBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tbaBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
